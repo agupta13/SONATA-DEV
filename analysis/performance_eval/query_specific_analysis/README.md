01ba004036a6dff1755e227e6d8780d149f6066c
Arpit Gupta <glex.qsd@gmail.com>
2017-08-19 10:22:05 -0400

    analysis for new connections query

---
+++ analysis/performance_eval/query_specific_analysis/README.md
@@ -0,0 +1,21 @@
+
+
+| Task Name| Analysis Completed|
+| ------------- | -------------|
+|SYN Flood Attack |  Yes| 
+|New Connections  | Yes| 
+|Completed Flows | No| 
+|Slowloris | No| 
+|Flow Distribution | No| 
+|Dup Acks| No| 
+|HH | No| 
+|SYN Flood Attack | No| 
+|Superspreader | No| 
+|SYN Flood Attack | No| 
+|Port Scan | No| 
+|DDoS | No| 
+|Traffic Asymmetry | No| 
+|SSh Bruteforce| No| 
+
+
+
---
+++ analysis/performance_eval/query_specific_analysis/new_connections.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#  Author:
+#  Arpit Gupta (arpitg@cs.princeton.edu)
+
+import pickle
+import time
+import datetime
+import math
+import json
+import numpy as np
+
+from sonata.query_engine.sonata_queries import *
+from sonata.core.training.utils import get_spark_context_batch, create_spark_context
+from sonata.core.integration import Target
+from sonata.core.refinement import get_refined_query_id, Refinement
+from sonata.core.training.hypothesis.hypothesis import Hypothesis
+
+
+def parse_log_line(logline):
+    return tuple(logline.split(","))
+
+
+def analyse_query(fname):
+    sc = create_spark_context()
+
+    packets = (sc.textFile(fname)
+               .map(parse_log_line)
+               # set ts = 1 for all packets
+               .map(lambda s: tuple([1] + (list(s[1:]))))
+               # .filter(lambda s: str(s[3]) == '43.236.76.200')
+               )
+
+    """
+    Schema:
+    ts,sIP,sPort,dIP,dPort,nBytes,proto,tcp.flags
+    """
+
+    n_syn = (packets
+             .filter(lambda s: str(s[-2]) == '6')
+             .filter(lambda s: str(s[-1]) == '2')
+             .map(lambda s: (s[3], 1))
+             .reduceByKey(lambda x, y: x + y)
+             )
+
+    print n_syn.take(5), n_syn.count()
+    data = n_syn.map(lambda s: s[1]).collect()
+    print "Mean", np.mean(data), "Median", np.median(data), "75 %", np.percentile(data, 75), \
+        "95 %", np.percentile(data, 95), "99 %", np.percentile(data, 99), "99.9 %", np.percentile(data, 99.9)
+
+if __name__ == '__main__':
+    fname = "/mnt/dirAB.out_00000_20160121080100.transformed.csv"
+    analyse_query(fname)
