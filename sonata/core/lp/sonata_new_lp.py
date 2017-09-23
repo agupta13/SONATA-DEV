b9f4bed29c2422fb8dbc74b54de57163a19ed041
Arpit <arpitg@cs.princeton.edu>
2017-09-22 22:49:05 -0400

    code sync

--- sonata/core/lp/sonata_new_lp.py
+++ sonata/core/lp/sonata_new_lp.py
@@ -280,7 +280,7 @@ def solve_sonata_lp(Q, query_2_tables, cost_matrix, qid_2_R, sigma_max, width_ma
     m.write(name + ".lp")
     # m.setParam(GRB.Param.OutputFlag, 0)
     # m.setParam(GRB.Param.LogToConsole, 0)
-    m.setParam(GRB.Param.TimeLimit, 300)
+    m.setParam(GRB.Param.TimeLimit, 600)
 
     m.optimize()
 
